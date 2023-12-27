20a1b571632a5e9816a9fcb3a5a6e3cb0b2bdf72
Esteban <estebanpiazza51@gmail.com>
2023-12-27 20:49:38 -0300

    cambio de ruleta

---
+++ ruleta/csv.js
@@ -0,0 +1,25 @@
+document.getElementById('terminar').addEventListener('click', function() {
+    // Obtener los datos del LocalStorage
+    var data = localStorage.getItem('formDataArray');
+    var jsonArray = JSON.parse(data);
+  
+    // Crear contenido CSV
+    var csvContent = "data:text/csv;charset=utf-8,";
+    csvContent += "Nombre,dni,Tel\n"; // Columnas del CSV
+  
+    // Añadir los datos del array al contenido CSV
+    jsonArray.forEach(function(row) {
+        var rowString = (row.nombre || '') + "," + (row.dni || '') + "," + (row.celular || '');
+        csvContent += rowString + "\n";
+    });
+  
+    // Crear un enlace para descargar el CSV
+    var encodedUri = encodeURI(csvContent);
+    var link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "datos.csv");
+    document.body.appendChild(link); // Necesario para Firefox
+  
+    // Descargar el archivo CSV
+    link.click();
+  });
