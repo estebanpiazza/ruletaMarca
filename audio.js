e23cde09687e1470a28ce859eca286afa7f9a2de
Esteban <estebanpiazza51@gmail.com>
2024-01-04 12:42:12 -0300

    musica

--- audio.js
+++ audio.js
@@ -1,19 +1,19 @@
 let audioElement0 = document.createElement('audio');
+
 audioElement0.setAttribute('src', 'beauteous-upbeat-electronic-162757.mp3');
+
 audioElement0.setAttribute('autoplay', 'autoplay');
+
 audioElement0.addEventListener('ended', function() {
     this.currentTime = 0;
     this.play();
 }, false);
-function myFunction() {
-    var x = document.getElementById("myAudio").duration;
-    document.getElementById("demo").innerHTML = x;
-}
-var myMusic = document.getElementById("music");
-function play() {
-    myMusic.play();
-}
 
-function pause() {
-    myMusic.pause();
-}
+
+let botonmusica = document.getElementById("musica")
+document.addEventListener("click", function(){
+    console.log("HOLA")
+    audioElement0.play()
+    botonmusica.style.display="none"
+})
+
